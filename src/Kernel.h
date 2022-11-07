e6ed5d9d1b92972ddb3a0e8e40622ba14ea7c0dc
samanthemum <sambhallam@gmail.com>
2022-11-07 11:24:05 -0600

    Fixed bugs
    * Fixed gradient math
    * Fixed neighborhood issues

--- src/Kernel.h
+++ src/Kernel.h
@@ -26,30 +26,31 @@ class Kernel {
 		}
 
 
-		static float spikyKernelGradient(const Particle& xi, const Particle& xj) {
+		static glm::vec3 spikyKernelGradient(const Particle& xi, const Particle& xj) {
+			// if we're less than the max radius, don't do anything
+			if (length(xi.getPosition() - xj.getPosition()) > SMOOTHING_RADIUS) {
+				return glm::vec3(0,0,0);
+			}
+
+			// otherwise
+			float outsideTerm = -45.0f / (M_PI * powf(SMOOTHING_RADIUS, 6.0f));
+			float insideTerm = powf(SMOOTHING_RADIUS - length(xi.getPosition() - xj.getPosition()), 2.0f);
+			return outsideTerm * insideTerm * normalize(xi.getPosition() - xj.getPosition());
+		}
+
+		// TODO: fix this function
+		static float polyKernelLaplacian(const Particle& xi, const Particle& xj) {
 			// if we're less than the max radius, don't do anything
 			if (length(xi.getPosition() - xj.getPosition()) > SMOOTHING_RADIUS) {
 				return 0;
 			}
 
 			// otherwise
-			float outsideTerm = -45.0f / (M_PI * powf(SMOOTHING_RADIUS, 6.0f));
-			float insideTerm = powf(SMOOTHING_RADIUS - length(xi.getPosition() - xj.getPosition()), 2.0f);
-			return outsideTerm * insideTerm;
-		}
-
-		static glm::vec3 polyKernelLaplacian(const Particle& xi, const Particle& xj) {
-			// if we're less than the max radius, don't do anything
-			if (length(xi.getPosition() - xj.getPosition()) > SMOOTHING_RADIUS) {
-				return glm::vec3(0.0f, 0.0f, 0.0f);
-			}
-
-			// otherwise
 			glm::vec3 r = xi.getPosition() - xj.getPosition();
 			float outsideTerm = 3.0f * 315.0f * length(r) * length(r) / (M_PI * powf(SMOOTHING_RADIUS, 9.0f) * 8.0f);
 			float insideTerm = powf(SMOOTHING_RADIUS, 2.0f) - powf(length(xi.getPosition() - xj.getPosition()), 2.0f);
-			glm::vec3 vectorTerm = glm::vec3((powf(r.y, 2.0f) + powf(r.z, 2.0f)) / powf(length(r), 3.0f), (powf(r.x, 2.0f) + powf(r.z, 2.0f)) / powf(length(r), 3.0f), (powf(r.y, 2.0f) + powf(r.x, 2.0f)) / powf(length(r), 3.0f));
-			return outsideTerm * insideTerm * vectorTerm;
+			// glm::vec3 vectorTerm = glm::vec3((powf(r.y, 2.0f) + powf(r.z, 2.0f)) / powf(length(r), 3.0f), (powf(r.x, 2.0f) + powf(r.z, 2.0f)) / powf(length(r), 3.0f), (powf(r.y, 2.0f) + powf(r.x, 2.0f)) / powf(length(r), 3.0f));
+			return outsideTerm * insideTerm;
 		}
 
 
